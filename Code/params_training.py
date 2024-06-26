6dcb8fbf33dad7041970d80db1642f8ee27061c2
samgende <sam.gende1415@gmail.com>
2024-06-26 17:17:22 +0200

    changes to get training to run on gpu

--- Code/params_training.py
+++ Code/params_training.py
@@ -1,11 +1,11 @@
 import datetime as dt 
-startTimes = [dt.datetime(2016,9,9,0,24,54,932000) ]
+startTimes = [dt.datetime(2016,9,4,23,55,54,932000) ]
 secondsPerFile = 60
 secondsPerWindowWidth =300
 #how much to shift the start time when performing cwt
 secondsPerWindowOffset =150
 xCorrMaxTimeLagSeconds = 3.0
-nFiless = [1444] 
+nFiless = [720] 
 outfilePath = '../scratch/'
 outfileListFile = 'inter_results/outfileTraining.txt'
 srcChList = 'sourceList.txt'
