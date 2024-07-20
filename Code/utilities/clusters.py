871affbe3fcaf3e890615d1b2eb1623f44e7ed5e
samgende <sam.gende1415@gmail.com>
2024-07-20 23:20:13 +0200

    make changes to clusters so that it runs with large datasets

--- Code/utilities/clusters.py
+++ Code/utilities/clusters.py
@@ -2,10 +2,8 @@ from sklearn import metrics
 
 
 def evaluate_cluster(Data, labels):
-    sillhouette_score = metrics.silhouette_score(Data, labels, metric='euclidean')
-    calinski_harabasz = metrics.calinski_harabasz_score(Data, labels)
-    daives_bouldn_score = metrics.davies_bouldin_score(Data, labels)
+    sillhouette_score = metrics.silhouette_score(Data, labels, metric='euclidean', sample_size=10000)
+    #calinski_harabasz = metrics.calinski_harabasz_score(Data, labels)
+    #daives_bouldn_score = metrics.davies_bouldin_score(Data, labels)
 
-    return {"sillhouette_score": sillhouette_score,
-            "CH_score": calinski_harabasz,
-            "DB_score": daives_bouldn_score}
+    return {"sillhouette_score": sillhouette_score}
