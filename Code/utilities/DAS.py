432c6f81e1bcce9c0d849139e3beb6d7a05421e6
samgende <sam.gende1415@gmail.com>
2024-07-27 20:20:11 +0200

    add one bit cross correlation to utilities

--- Code/utilities/DAS.py
+++ Code/utilities/DAS.py
@@ -21,3 +21,9 @@ def open_H5_file(file_path):
     tmp = file_path.split('.h5')[0].split('/')[-1].split('_')[2:5]  #a bit convoluted. 
     start_datetime = datetime.datetime.strptime(tmp[1] + ' ' + tmp[2],'%Y%m%d %H%M%S.%f')  # in datetime
     return DAS_data, start_datetime
+
+def one_bit_cross_cor(source, reciever, offset):
+    offset
+    source_ones = np.sign(source)
+    reciever_ones= np.sign(reciever)
+    return np.sum(source_ones[:-offset][-1] * reciever_ones[offset:], axis=0)
