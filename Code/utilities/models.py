df6af22d96845b6295bb586d0cd2438e2dd013e6
samgende <sam.gende1415@gmail.com>
2024-07-29 02:22:35 +0200

    add autoencoder to uttilities

---
+++ Code/utilities/models.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Autoencoder_v1(nn.Module):
+    def __init__(self, encoding_dim, input_dim):
+        super(Autoencoder_v1, self).__init__()
+        ## encoder ##
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 32),
+            nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(32, 16),
+            nn.Tanh(),
+            nn.Linear(16, encoding_dim)
+        )
+        ## decoder ##
+        self.decoder =   self.decoder = nn.Sequential(
+            nn.Linear(encoding_dim, 16),
+            nn.Tanh(),
+            nn.Linear(16, 32),
+            nn.Tanh(),
+            nn.Linear(32,input_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.float32)
+        # define feedforward behavior 
+        # and scale the *output* layer with a sigmoid activation function
+        
+        # pass x into encoder
+        out = F.relu(self.encoder(x))
+        # pass out into decoder
+        out = self.decoder(out)
+        
+        return out
+    
+    def encode(self, x ):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.float32)
+        return self.encoder(x)
