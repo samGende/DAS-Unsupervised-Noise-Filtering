66109fb4793771cebab0706b5f14b2f5e337f142
samgende <sam.gende1415@gmail.com>
2024-07-30 17:16:53 +0200

    changes for gpu

--- Code/run_cwt_synthetic_data.py
+++ Code/run_cwt_synthetic_data.py
@@ -12,9 +12,9 @@ file_list.sort()
 
 n_samples = 3000
 
-sample = torch.load(DAS_Directory +'/' +file_list[0])[:,:n_samples]
+sample = torch.load(DAS_Directory +'/' +file_list[0])
+print(file_list[0])
 print(sample.shape)
-assert(0)
 
 #parameters for cwt
 dt =0.5
@@ -32,23 +32,11 @@ space_log = np.logspace(np.log10(minSpaceFrq), np.log10(maxSpaceFrq), n_features
 time_scales= cwt.get_scales(dt, dj, w0, n_samples)
 
 
-n_channels = sample.shape[0]
-samples_per_second = 50
-samples_per_sub_sample = 1
-space_log = np.logspace(np.log10(minSpaceFrq), np.log10(maxSpaceFrq), n_features)
-time_scales= cwt.get_scales(dt, dj, w0, n_samples)
-
-transform_dir = "./Data/synthetic-DAS/train-syntheticDAS/CWT-edDAS"
+transform_dir = "./Data/synthetic-DAS/train-syntheticDAS/CWT-edDAS/subsampled"
 print(len(file_list))
 for index, file in tqdm(enumerate(file_list)):
     sample = np.array(torch.load(DAS_Directory +'/' +file))
-    sample2 = sample[:,n_samples:]
-    sample1= sample[:,:n_samples]
-    transform1 = cwt.transform_window(sample1, n_channels, samples_per_second, samples_per_sub_sample, space_log, time_scales, start_window=0, end_window=n_channels, window_length=n_samples, subsampling = False, derivative = False)
-    transform2 = cwt.transform_window(sample2, n_channels, samples_per_second, samples_per_sub_sample, space_log, time_scales, start_window=0, end_window=n_channels, window_length=n_samples, subsampling = False, derivative = False)
-    torch.save(transform1, transform_dir +'/'+ f'transform{index:04d}_window1.pt')
-    torch.save(transform2, transform_dir +'/'+ f'transform{index:04d}_window2.pt')
+    transform = cwt.transform_window(sample, n_channels, samples_per_second, samples_per_sub_sample, space_log, time_scales, start_window=0, end_window=5950, window_length=238, subsampling = True, derivative = True)
+    torch.save(transform, transform_dir +'/'+ f'transform{index:04d}_window2.pt')
     if(index == 34):
         break
-
-
