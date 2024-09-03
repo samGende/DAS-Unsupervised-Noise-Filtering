e5502356d478d711f92c09f3c31b04aaaeaacdd9
samgende <sam.gende1415@gmail.com>
2024-09-03 10:16:40 +0200

    add complex model

--- Code/utilities/models.py
+++ Code/utilities/models.py
@@ -1,6 +1,33 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn import Module
 import torch
+import complexPyTorch.complexFunctions as complexFunctions
+from complexPyTorch.complexLayers import ComplexLinear, ComplexReLU
+from complexPyTorch.complexFunctions import complex_relu
+
+
+def complex_sigmoid(inp):
+    return F.sigmoid(inp.real).type(torch.complex64) + 1j * F.sigmoid(inp.imag).type(
+        torch.complex64
+    )
+
+
+def complex_tanh(inp):
+    return F.tanh(inp.real).type(torch.complex64) + 1j * F.tanh(inp.imag).type(
+        torch.complex64
+    )
+
+class ComplexSigmoid(Module):
+    @staticmethod
+    def forward(inp):
+        return complex_sigmoid(inp)
+    
+class ComplexTanh(Module):
+    @staticmethod
+    def forward(inp):
+        return complex_tanh(inp)
+
 
 class Autoencoder_v1(nn.Module):
     def __init__(self, encoding_dim, input_dim):
@@ -45,4 +72,97 @@ class Autoencoder_v1(nn.Module):
     def decode(self, x):
         if(not torch.is_tensor(x)):
             x = torch.tensor(x, dtype=torch.float32)
+        return self.decoder(x)
+
+    
+    
+    
+class Autoencoder_v2(nn.Module):
+    def __init__(self, encoding_dim, input_dim):
+        super(Autoencoder_v2, self).__init__()
+        ## encoder ##
+        self.encoder = nn.Sequential(
+            Complex(input_dim, 32),
+            nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(32, 16),
+            nn.Tanh(),
+            nn.Linear(16, encoding_dim)
+        )
+        ## decoder ##
+        self.decoder =   self.decoder = nn.Sequential(
+            nn.Linear(encoding_dim, 16),
+            nn.Tanh(),
+            nn.Linear(16, 32),
+            nn.Tanh(),
+            nn.Linear(32,input_dim),
+        )
+
+    def forward(self, x):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.float32)
+        # define feedforward behavior 
+        # and scale the *output* layer with a sigmoid activation function
+        
+        # pass x into encoder
+        out = F.relu(self.encoder(x))
+        # pass out into decoder
+        out = self.decoder(out)
+        
+        return out
+    
+    def encode(self, x ):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.float32)
+        return self.encoder(x)
+    
+    def decode(self, x):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.float32)
+        return self.decoder(x)
+
+    
+class Autoencoder_v3(nn.Module):
+    def __init__(self, encoding_dim, input_dim):
+        super(Autoencoder_v3, self).__init__()
+        ## encoder ##
+        self.encoder = nn.Sequential(
+            ComplexLinear(input_dim, 32),
+            ComplexReLU(),
+            ComplexTanh(),
+            ComplexLinear(32, 16),
+            ComplexTanh(),
+            ComplexLinear(16, encoding_dim)
+        )
+        ## decoder ##
+        self.decoder =   self.decoder = nn.Sequential(
+            ComplexLinear(encoding_dim, 16),
+            ComplexTanh(),
+            ComplexLinear(16, 32),
+            ComplexTanh(),
+            ComplexLinear(32,input_dim),
+            ComplexSigmoid()
+        )
+
+    def forward(self, x):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.complex64)
+        # define feedforward behavior 
+        # and scale the *output* layer with a sigmoid activation function
+
+        # pass x into encoder
+        out = complex_relu(self.encoder(x))
+        # pass out into decoder
+        out = self.decoder(out)
+
+        return out
+
+    def encode(self, x ):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.complex64)
+        return self.encoder(x)
+
+    def decode(self, x):
+        if(not torch.is_tensor(x)):
+            x = torch.tensor(x, dtype=torch.complex64)
         return self.decoder(x)
